fc214841b9a251f1dd2630b5b7bf0ff84cedaae0
Sofia Lunkes <sofia.big@live.com>
2016-12-21 22:16:05 -0200

    Menu de visualização do Hotel

--- agencia-viagem-mvc/Models/Hotel.cs
+++ agencia-viagem-mvc/Models/Hotel.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace agencia_viagem_mvc.Models {
     public class Hotel {
         public long Id { get; set; }
+        [Display(Name ="Nome")]
         public string NomeFantasia { get; set; }
         public string Cnpj { get; set; }
         public string Telefone { get; set; }
