daff4cc4c5898a814ec68a74e6fb96f90d3255c6
Sofia Lunkes <sofia.big@live.com>
2016-12-24 17:52:04 -0200

    remodelando relacionamento - pacote possui viagem

--- agencia-viagem-mvc/Models/Viagem.cs
+++ agencia-viagem-mvc/Models/Viagem.cs
@@ -4,15 +4,14 @@ using System.Linq;
 using System.Web;
 
 namespace agencia_viagem_mvc.Models {
-    public class Pacote {
+    public class Viagem {
         public long Id { get; set; }
-        public string Descricao { get; set; }
+        public string Nome { get; set;}
+
         public int QuantidadeDias { get; set; }
 
-        public List<long?> ClientesId { get; set; }
         public long? HotelId { get; set; }
 
-        public List<Cliente> Clientes { get; set; }
         public Hotel Hotel { get; set; }
     }
 }
