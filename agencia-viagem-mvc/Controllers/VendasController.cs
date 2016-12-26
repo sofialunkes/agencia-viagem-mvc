f27455650b1f22f717186ad1db38ffc1487daaa0
Sofia Lunkes <sofia.big@live.com>
2016-12-26 02:19:36 -0200

    Adicionando uma nova compra

--- agencia-viagem-mvc/Controllers/VendasController.cs
+++ agencia-viagem-mvc/Controllers/VendasController.cs
@@ -3,15 +3,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 using System.Web.Mvc;
+using agencia_viagem_mvc.Models;
 
 namespace agencia_viagem_mvc.Controllers {
     public class VendasController : Controller {
         private EFContext context = new EFContext();
         // GET: Vendas
         public ActionResult Index() {
-            return View(context.Vendas.OrderBy(
-               p => p.DataAquisicao));
+            var vendas = context.Vendas.Include(c => c.Cliente).Include(p => p.Pacote).OrderByDescending(n => n.DataAquisicao);
+            return View(vendas);
         }
 
         // GET: Vendas/Details/5
@@ -21,18 +23,20 @@ namespace agencia_viagem_mvc.Controllers {
 
         // GET: Vendas/Create
         public ActionResult Create() {
+            ViewBag.ClienteId = new SelectList(context.Clientes.OrderBy(b => b.Nome), "Id","Nome");
+            ViewBag.PacoteId = new SelectList(context.Pacotes.OrderBy(b => b.Nome), "Id","Nome");
             return View();
         }
 
         // POST: Vendas/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection) {
+        public ActionResult Create(Venda venda) {
             try {
-                // TODO: Add insert logic here
-
+                context.Vendas.Add(venda);
+                context.SaveChanges();
                 return RedirectToAction("Index");
             } catch {
-                return View();
+                return View(venda);
             }
         }
 
@@ -43,7 +47,7 @@ namespace agencia_viagem_mvc.Controllers {
 
         // POST: Vendas/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection) {
+        public ActionResult Edit(int id, Venda venda) {
             try {
                 // TODO: Add update logic here
 
@@ -60,7 +64,7 @@ namespace agencia_viagem_mvc.Controllers {
 
         // POST: Vendas/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection) {
+        public ActionResult Delete(int id, Venda venda) {
             try {
                 // TODO: Add delete logic here
 
