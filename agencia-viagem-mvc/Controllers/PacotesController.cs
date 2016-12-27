e4b1e790fe7e4031f945cf3fe9df0eabdfee77e8
Sofia Lunkes <sofia.big@live.com>
2016-12-27 01:29:22 -0200

    Adicionar novo pacote com associação a um Hotel

--- agencia-viagem-mvc/Controllers/PacotesController.cs
+++ agencia-viagem-mvc/Controllers/PacotesController.cs
@@ -13,20 +13,25 @@ namespace agencia_viagem_mvc.Controllers {
         private EFContext context = new EFContext();
         // GET: Pacotes
         public ActionResult Index() {
-            return View(context.Pacotes.OrderBy(
-                p=> p.Nome));
+            var pacotes = context.Pacotes.Include(h => h.Hotel).OrderBy(n => n.Id);
+            return View(pacotes);
         }
 
         public ActionResult Create() {
+            ViewBag.HotelId = new SelectList(context.Hoteis.OrderBy(b => b.Id), "Id", "NomeFantasia");
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(Pacote pacote) {
-            context.Pacotes.Add(pacote);
-            context.SaveChanges();
-            return RedirectToAction("Index");
+            try {
+                context.Pacotes.Add(pacote);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            } catch {
+                return View(pacote);
+            }
         }
 
         public ActionResult Edit(long? id) {
