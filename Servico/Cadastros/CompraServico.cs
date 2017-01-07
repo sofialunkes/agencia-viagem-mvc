d9fd055ce2306c7f621c5e58c17efe0b11d0a8cf
Sofia Lunkes <sofia.big@live.com>
2017-01-06 23:54:35 -0200

    Camada de Servico de Compra

---
+++ Servico/Cadastros/CompraServico.cs
@@ -0,0 +1,30 @@
+﻿using Persistencia.DAL.Cadastros;
+using Modelo.Cadastros;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servico.Cadastros {
+    public class CompraServico {
+        private CompraDAL compraDAL = new CompraDAL();
+
+        public IQueryable<Compra> ObterCompraPorMaiorData() {
+            return compraDAL.ObterCompraPorMaiorData();
+        }
+
+        public Compra ObterCompraPorId(long id) {
+            return compraDAL.ObterCompraPorId(id);
+        }
+
+        public void GravarCompra(Compra compra) {
+            compraDAL.GravarCompra(compra);
+        }
+
+        public Compra EliminarCompraPorId(long id) {
+            return compraDAL.EliminarCompraPorId(id);
+        }
+
+    }
+}
